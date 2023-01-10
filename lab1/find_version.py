32c71845489b9308cb12eaf8c6d4ca90ce226fdf
The Cat <56602118+thecatinbox@users.noreply.github.com>
2023-01-09 18:42:34 -0700

    add curl task 5

--- lab1/find_version.py
+++ lab1/find_version.py
@@ -1,5 +1,11 @@
 import requests
 
+# Print out the version of the requests library
 print(requests.__version__)
+
+# GET the Google homepage
+print(requests.get("http://www.google.com/"))
+
+# Download from GitHub and print out its own source code from GitHub
 x = requests.get("https://raw.githubusercontent.com/thecatinbox/CMPUT404-lab/main/lab1/find_version.py")
 print(x.text)
