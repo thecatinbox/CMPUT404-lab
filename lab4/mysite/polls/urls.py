059e2415f6945578a5c988606707426542f073be
Vivian <catinboxx@gmail.com>
2023-01-30 17:42:41 -0700

    lab 4 - allow arbitrary alphabetic characters

--- lab4/mysite/polls/urls.py
+++ lab4/mysite/polls/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<question_id>/vote/', views.vote, name='vote'),
 ]
