25bbce7a965f5a34e37fdf4087ed780f5c51f08f
Vivian <catinboxx@gmail.com>
2023-01-30 18:42:59 -0700

    lab 4 - Django Rest Framework
    Serializing and Deserializing Queries https://www.django-rest-framework.org/tutorial/1-serialization/

--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -3,7 +3,31 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import QuestionSerializer
+
 from .models import Choice, Question
+
+@api_view(['GET'])
+def get_questions(request):
+    """
+    Get the list of questions on our website
+    """
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def update_question(request, pk):
+    """
+    Get the list of questions on our website
+    """
+    questions = Question.objects.get(id=pk)
+    serializer = QuestionSerializer(questions, data=request.data, partial=True)
+    if serializer.is_valid():
+        return Response(serializer.data)
+    return Response(status=400, data=serializer.errors)
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
