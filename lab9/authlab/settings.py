ec4b11c3a6b1b6e0622b524eb3c84d60e551d76c
Vivian <catinboxx@gmail.com>
2023-03-06 17:47:12 -0700

    Update Authentication Token

--- lab9/authlab/settings.py
+++ lab9/authlab/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'client.apps.ClientConfig',
+    'rest_framework.authtoken', 
+    'client.apps.ClientConfig'
 ]
 
 MIDDLEWARE = [
@@ -128,5 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     )
 }
